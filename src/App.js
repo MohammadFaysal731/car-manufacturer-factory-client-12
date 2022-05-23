866902ac48c1cae34e7db542744229056ce04f32
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-23 13:14:03 +0600

    install react roter and set routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Portfolio from './Pages/Portfolio/Portfolio';
 import Header from './ShearedPages/Header/Header';
+import NotFound from './ShearedPages/NotFound/NotFound';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
+    <div className="">
+      <Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/protfolio' element={<Portfolio></Portfolio>}></Route>
+          <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
+        </Routes>
+      </Header>
 
     </div>
   );
--- src/ShearedPages/Header/Header.js
+++ src/ShearedPages/Header/Header.js
@@ -1,19 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ children }) => {
+    const menuItems = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/protfolio'>PortFolio</Link></li>
+        <li><Link to='/blogs'>Blogs</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+    </>
     return (
-        <div>
-            <h1>heeadr s </h1>
-            <button class="btn">Button</button>
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row">
-                    <img src="https://api.lorem.space/image/movie?w=260&h=400" class="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
-                        <h1 class="text-5xl font-bold">Box Office News!</h1>
-                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+        <div className="drawer ">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+
+                <div className="w-full navbar bg-primary text-white">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                        </label>
+                    </div>
+                    <div className="flex-1 px-2 mx-2  ">CAR PARTS MANUFACTURE FACTORY</div>
+                    <div className="flex-none hidden lg:block">
+                        <ul className="menu menu-horizontal gap-3 ">
+                            {menuItems}
+                        </ul>
                     </div>
                 </div>
+                {children}
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" className="drawer-overlay"></label>
+                <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+                    {menuItems}
+                </ul>
+
             </div>
         </div>
     );
