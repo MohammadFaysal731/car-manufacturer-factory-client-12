f583552027f47126c685687c70d2d63e63c78674
MohammadFaysal731 <www.faysal731@gmail.com>
2022-05-26 23:51:20 +0600

    create delete system

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useQuery } from 'react-query';
 import auth from '../../../firebase.init';
 import Loading from '../../../ShearedPages/Loading/Loading';
 import OrderRow from '../OrderRow/OrderRow';
@@ -8,19 +9,11 @@ import OrderRow from '../OrderRow/OrderRow';
 
 
 const MyOrders = () => {
-    const [user, loading] = useAuthState(auth);
-    const [orders, setOrders] = useState([]);
+    const [user, userLoading] = useAuthState(auth);
 
+    const { data: orders, isLoading, refetch } = useQuery(['orders', user?.email], () => fetch(`http://localhost:5000/order?email=${user?.email}`, {}).then(res => res.json()))
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/order?email=${user?.email}`)
-            .then(res => res.json())
-            .then(data => {
-                setOrders(data);
-            })
-    }, [user?.email])
-
-    if (loading) {
+    if (userLoading || isLoading) {
         return <Loading></Loading>
     }
 
@@ -31,7 +24,6 @@ const MyOrders = () => {
 
             <div className="overflow-x-auto">
                 <table className="table w-full">
-
                     <thead>
                         <tr>
                             <th>Sl</th>
@@ -43,16 +35,18 @@ const MyOrders = () => {
                     </thead>
                     <tbody>
                         {
-                            orders.map((order, index) => <OrderRow
+                            orders?.map((order, index) => <OrderRow
                                 order={order}
                                 key={order._id}
                                 index={index}
+                                refetch={refetch}
                             ></OrderRow>)
                         }
-
                     </tbody>
                 </table>
+
             </div>
+
 
         </div>
     );
--- src/Pages/Dashboard/OrderRow/OrderRow.js
+++ src/Pages/Dashboard/OrderRow/OrderRow.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const OrderRow = ({ order, index }) => {
-    const { productName, productQuantity } = order;
+const OrderRow = ({ order, index, refetch }) => {
+    const { _id, productName, productQuantity } = order;
+
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/order/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                refetch(data);
+                toast.success('Product Will Be Deleted Success Fully')
+            });
+    }
+
     return (
-        <tr>
+        <tr className='uppercase'>
             <td>{index + 1}</td>
             <td>{productName}</td>
             <td>{productQuantity}/Pcs</td>
-            <td><button className="btn btn-xs btn-primary btn-outline">Delete</button></td>
+            <td>
+
+                <button onClick={() => handleDelete(_id)} className="btn btn-xs btn-primary btn-outline">Delete</button>
+            </td>
             <td><button className="btn btn-xs btn-primary btn-outline">Pay</button></td>
         </tr>
     );
